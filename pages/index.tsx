d49b0b743389fd1b5301aa76b7b4a4e2d62e656a
Abhay Nath <abhaynath88@gmail.com>
2020-09-25 20:15:09 +0530

    post api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import Axios from "axios";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
+const callApi = async () => {
+  console.log("--call api --");
+  let resp = await Axios.post("/user");
+  console.log("--response : ", resp.data);
+};
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+    <button onClick={callApi}>API call</button>
     <p>
       <Link href="/about">
         <a>About</a>
